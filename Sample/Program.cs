95e5ce6a704b530b13421bd112c756c34b80126e
Juha <kukiyomenaisapp@gmail.com>
2023-07-12 10:02:08 +0900

    整形

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,18 +1,16 @@
 ﻿using JuhaKurisu.PostProcessSystem;
 using Sample;
 
-PostProcessor<int> postProcessor = new PostProcessor<int>();
-postProcessor.PostProcessors = new IPostProcessor<int>[]
+var postProcessor = new PostProcessor<int>
 {
-    new TestAdder(),
-    new TestMultiplier(),
+    PostProcessors = new IPostProcessor<int>[]
+    {
+        new TestAdder(),
+        new TestMultiplier()
+    }
 };
 
 if (int.TryParse(Console.ReadLine(), out var value))
-{
-    Console.WriteLine(postProcessor.PostProcess(value));   
-}
+    Console.WriteLine(postProcessor.PostProcess(value));
 else
-{
     Console.WriteLine("Parse Error!!!!");
-}
