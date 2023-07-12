e1ddfada3d29c2affb133413487e91ba890ee869
Juha <kukiyomenaisapp@gmail.com>
2023-07-12 10:01:22 +0900

    PostProcessor内のPostProcessを実装

--- PostProcessSystem/PostProcessor.cs
+++ PostProcessSystem/PostProcessor.cs
@@ -5,14 +5,16 @@ namespace JuhaKurisu.PostProcessSystem;
 public class PostProcessor<T> : IPostProcessor<T>
 {
     public readonly IReadOnlyCollection<ExecutionOrderConstraint> Constraints;
+    public IPostProcessor<T>[] PostProcessors;
 
     public PostProcessor(params ExecutionOrderConstraint[] constraints)
     {
         Constraints = new ReadOnlyCollection<ExecutionOrderConstraint>(constraints);
+        PostProcessors = Array.Empty<IPostProcessor<T>>();
     }
 
     public T PostProcess(T value)
     {
-        return value;
+        return PostProcessors.Aggregate(value, (current, postProcessor) => postProcessor.PostProcess(current));
     }
 }
