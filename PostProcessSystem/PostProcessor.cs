699af4110ba44ea15bff5cb6e788789706f4c701
Juha <kukiyomenaisapp@gmail.com>
2023-07-12 09:45:33 +0900

    PostProcessorにConstraintsを保持するように

--- PostProcessSystem/PostProcessor.cs
+++ PostProcessSystem/PostProcessor.cs
@@ -1,7 +1,16 @@
+using System.Collections.ObjectModel;
+
 namespace JuhaKurisu.PostProcessSystem;
 
 public class PostProcessor<T> : IPostProcessor<T>
 {
+    public readonly IReadOnlyCollection<ExecutionOrderConstraint> Constraints;
+
+    public PostProcessor(params ExecutionOrderConstraint[] constraints)
+    {
+        Constraints = new ReadOnlyCollection<ExecutionOrderConstraint>(constraints);
+    }
+
     public T PostProcess(T value)
     {
         return value;
